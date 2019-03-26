08d99e815cf518dc540cfd7703192c346a142c62
DoMy <domy@DoMys-MacBook-Pro.local>
2019-03-26 21:27:32 +0300

    AddProduct, ALl APIs except Add product

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import { Text, View, Alert, Picker } from 'react-native';
 import { Card, Button, Section, TextInputt, CardSection } from './common';
-export default class AddProduct extends Component {
 
+export default class AddProduct extends Component {
   state = { title: '', product_title: '',
   product_description_en: '',
   product_description: '',
-  auth: '40822bf8da8c608d5aea268d69db005140bd39a7'}
-
-
-
+  category: 'None',
+  itemValue: '',
+  categories: []
+}
+  componentWillMount() {
+    this.getProductsIds();
+  }
   getAuth(){
     fetch('https://haaz.exp-pv.com/api/get_auth', {
       method: 'GET',
-      // domy abo klbfgh
       headers: {
         Accept: 'application/json', 'Content-Type': 'application/json',
       }}).then((response) => response.json())
@@ -29,7 +31,7 @@ export default class AddProduct extends Component {
     SendProductToBackendAndSaveResults() {
       fetch('https://haaz.exp-pv.com/api/create_product', {
         method: 'POST',
-        body: JSON.stringify({ auth: '40822bf8da8c608d5aea268d69db005140bd39a7', ...this.state   }),
+        body: JSON.stringify({ auth: '57b126bcc851304b0e80c4c336074a690cc70c66', ...this.state }),
       }).then((response) => {
         const res = response.status;
         res === 200 ? console.log('it worksss!!!') : console.log(`Fuck the STATUS # is ${res}`);
@@ -40,22 +42,35 @@ export default class AddProduct extends Component {
       });
     }
 
-    //   getProductsIds(){
-    //     fetch('https://haaz.exp-pv.com/api/get_product_type', {
-    //
-    //     }).then((response) => {
-    //       Object.keys(response.json())
-    //       .map((key, index) => {
-    //
-    //         console.log(response.json()[key]);
-    //
-    //       })
-    //
-    //   })
-    //   .catch((error) => {
-    //     console.error(error);
-    //   });
-    // }
+      getProductsIds(){
+        fetch('https://haaz.exp-pv.com/api/get_product_type', {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json', 'Content-Type': 'application/json',
+          }}).then((response) => response.json())
+          .then((responseJson) => {
+            this.setState({ categories: responseJson })
+            return;
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+    }
+    ShowList() {
+      return (
+      <Picker
+            style={{ flex: 1, height: 44 }}
+            itemStyle={{ height: 44, borderWidth: 0 }}
+            selectedValue={this.state.category}
+            onValueChange={category => this.setState({ category })}
+      >
+            {this.state.categories.map((item, index) => {
+              return <Picker.Item label={item.cat_name_en} value={index} />
+            })}
+            </Picker>
+          )
+    }
+    categories = [];
 
     // ShowList(){
     //   if (this.state.product_id.length > 0) {
@@ -67,6 +82,9 @@ export default class AddProduct extends Component {
     //       selectedValue={this.state.category}
     //       onValueChange={category => this.setState({ category })}
     //       >
+    //        arr.map((item)=> (
+    //        <Picker.Item label="cat_name_en" value=item.cat_name_en" />
+    //        ))
     //       <Picker.Item label="اظهر الكل" value="all" />
     //       <Picker.Item label="مطاعم" value="resturant" />
     //       <Picker.Item label="فنادق" value="hotel" />
@@ -127,10 +145,10 @@ export default class AddProduct extends Component {
         </Section>
 
         <Section>
-        <Button whenPress={() => (this.getAuth())}>
+        <Button whenPress={() => (this.getProductsIds())}>
         تابع !
         </Button>
-        <Button whenPress={() => (Alert.alert(this.state.responseJson))}>
+        <Button whenPress={() => (console.log(this.categories))}>
         Check Here !
         </Button>
         </Section>
@@ -139,7 +157,15 @@ export default class AddProduct extends Component {
     }
     render() {
       return (
-        this.Screen()
+        <View>
+        <Card>
+        {this.ShowList()}
+        </Card>
+        <View style={{ marginTop: 50 }}>
+        {this.Screen()}
+        </View>
+      </View>
+
 
       );
     }
