69ab5389f65d65095e586404d54a08cde8d3239b
domy <domy1@outlook.com>
2019-05-07 00:24:25 +0300

    domy's work

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import AccountPage from './components/AccountPage.js';
 import GeolocationExample from './components/GeolocationExample.js';
-import GetLocationPage from './components/GetLocationPage.js';
-
+import Basic from './components/Basic.js';
+import RouterComponent from './Router';
 
 class App extends Component {
 
     render() {
         return (
-            <View>
-                {/*<GeolocationExample/>*/}
-                {/*<AccountPage />*/}
-                <GetLocationPage />
+          <View style={{ flex: 1, marginBottom: 10 }}>
+
+                <RouterComponent />
             </View>
         );
     }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
+import { Dimensions, View } from 'react-native';
+import SideMenu from 'react-native-side-menu';
 import { Card, Button, Section, TextInputt } from './common';
+import Menu from './Menu';
+
+const window = Dimensions.get('window');
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+
+    this.state = {
+      isOpen: false,
+      selectedItem: 'About',
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  updateMenuState(isOpen) {
+    this.setState({ isOpen });
+  }
+
+  onMenuItemSelected = item =>
+    this.setState({
+      isOpen: false,
+      selectedItem: item,
+    });
 
   render() {
-	    return (
+    const menu = <Menu onItemSelected={this.onMenuItemSelected} />;
 
+	    return (
+        <View>
+        <View
+        style={{ width: window.width,
+        height: window.height }}
+        />
+        <SideMenu
+          menu={menu}
+          isOpen={this.state.isOpen}
+          onChange={isOpen => this.updateMenuState(isOpen)}
+        >
         			<Card>
               <Section>
                 <TextInputt
@@ -52,6 +94,8 @@ class Login extends Component {
                 </Button>
         				</Section>
         			</Card>
+              </SideMenu>
+              </View>
 	    );
 	  }
 	}
